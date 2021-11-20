b49a593c70092a56a792c4f2e92607d8c22e9b0c
Sully Bck <55058639+SullyvanLB@users.noreply.github.com>
2021-11-20 11:08:27 +0100

    Add files via upload

---
+++ scripts/montage.js
@@ -0,0 +1,6 @@
+//boite de dialogue affichée uniquement à la premieère ouverture de la page
+var alerted = localStorage.getItem('alerted') || '';
+    if (alerted != 'yes') {
+     alert("Bienvenue sur ma page espace libre, dans laquelle j'ai décidé de présenter le montage vidéo");
+     localStorage.setItem('alerted','yes');
+    }
